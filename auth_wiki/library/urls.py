6bb151a1fb5c945694458c80cb6403a7ac3ee92d
Gorgeous T <tomisinerinle4@gmail.com>
2022-12-13 00:10:26 +0100

    [feat] :- Created views and urls for  auth_wiki library app

--- auth_wiki/library/urls.py
+++ auth_wiki/library/urls.py
@@ -2,4 +2,11 @@ from django.urls import path
 from library.views import *
 
 urlpatterns = [
+    path('', home_view, name='home'),
+    path('about/', about_view, name='about'),
+    path('faq/', faq_view, name='faq'),
+    path('library/', library_view, name='library'),
+    path("<int:pk>/create_comment", CommentAPIView.as_view(), name="create-comment"),
+    path("comment/<int:pk>/create_upvote", UpVoteAPIView.as_view(), name="create-upvote"),
+    path("comment/<int:pk>/create_downvote", DownVoteAPIView.as_view(), name="create-downvote")
 ]
