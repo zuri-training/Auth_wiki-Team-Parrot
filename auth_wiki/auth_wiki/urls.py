e9b7dfd92e607636cb3fafc1e4141f701aeb2749
Gorgeous T <tomisinerinle4@gmail.com>
2022-12-14 14:39:12 +0100

    configuring apps details

--- auth_wiki/auth_wiki/urls.py
+++ auth_wiki/auth_wiki/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('acct/', include('account.urls')),
     path('', include('library.urls')),
     path('api_auth/', include('rest_framework.urls', namespace='rest_framework')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-]  
