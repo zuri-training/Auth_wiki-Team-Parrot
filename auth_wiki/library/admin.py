905d8a193a780699aee3002b9eaf388624248c38
Gorgeous T <tomisinerinle4@gmail.com>
2022-12-16 21:03:19 +0100

    configuring auth_wiki project

--- auth_wiki/library/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-
-admin.site.register(Faq)
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Category)
