5db6c44b970a7885cc6088377645f7ff0b2905cc
Gorgeous T <tomisinerinle4@gmail.com>
2022-12-16 10:07:46 +0100

    working with permission,designs and logic of the library app

--- auth_wiki/library/views.py
+++ auth_wiki/library/views.py
@@ -1,23 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
+from .forms import *
 from .serializers import *
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
-# # testing testing
-# from django.shortcuts import render, get_object_or_404, redirect
-# from library.models import *
-# from account.models import Profile
-# from django.http import HttpResponseRedirect
-# from account.models import User
-# from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-# from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-# from django.db.models import Count
-# from .forms import NewCommentForm
-# from django.contrib.auth.decorators import login_required
-
-# testing 1..2
+from django.contrib.auth.decorators import login_required
+from django.db.models.query_utils import Q
 
 
 # Create your views here.
@@ -48,6 +40,51 @@ def library_view(request):
     data = Post.objects.all()
     context = {'post_data':data}
     return render(request, template_name, context=context)
+
+
+def documentation_view(request):
+    template_name = 'main/documentation.html'
+    context = {}
+    return render(request, template_name, context=context)
+
+
+
+@login_required(login_url='account:register')
+def detail_view(request, slug):
+    template_name = 'main/details_page.html'
+    requested_post = get_object_or_404(Post, slug=slug)
+    comments = requested_post.comments.all().order_by('-created_on')
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = requested_post
+            comment.user = request.user
+            comment.save()
+            return HttpResponseRedirect(reverse('details', args=[slug]))
+
+
+
+    context = {
+        'detail_post': requested_post,
+        'comments_data': comments,
+        'comment_form': form,
+    }
+
+    return render(request, template_name, context)
+
+
+def search_library(request):
+    search_q = request.GET.get('q')
+    filtered_object = Post.objects.filter(Q(title__contains=search_q) | Q(description__contains=search_q))
+    context = {
+        'post_data': filtered_object,
+        'length_of_data': len(filtered_object),
+        'searched_q': search_q,
+    }
+    return render(request, 'main/search.html', context)
+    
 
 
 
