e3ed9354e5bdb2544b274fcdd64b03742149d336
Gorgeous T <tomisinerinle4@gmail.com>
2022-12-16 17:49:17 +0100

    setting app for deployment

--- auth_wiki/auth_wiki/settings.py
+++ auth_wiki/auth_wiki/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'DEFAULT_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
