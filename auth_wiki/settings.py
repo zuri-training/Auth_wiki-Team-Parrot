29d112abbb6c6d9ae7189f609f464f25048e5a17
Gorgeous T <tomisinerinle4@gmail.com>
2022-12-17 08:43:15 +0100

    Implemented White Noise

--- auth_wiki/settings.py
+++ auth_wiki/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 
     # installed apps
     'account',
@@ -50,8 +51,13 @@ INSTALLED_APPS = [
     'allauth',  
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    
     #'allauth.account',
     #'django.contrib.sites',
+
+    # whitenoise
+    'whitenoise.runserver_nostatic',  
+
 ]
 
 MIDDLEWARE = [
@@ -62,6 +68,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'auth_wiki.urls'
@@ -130,14 +138,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # STATICFILES_DIRS =  [
 #     os.path.join(BASE_DIR, 'static')
 # ]
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Media
 
 MEDIA_URL = '/media/'
